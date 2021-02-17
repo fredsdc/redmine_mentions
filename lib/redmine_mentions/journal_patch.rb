3c2436c8bc5c4b906bcebeaeb9230174d7c4859c
Frederico Camara <fredsdc@gmail.com>
2021-02-17 14:09:44 -0300

    JS fixes (jkraemer @ jk@jkraemer.net)
    - JS escaping of user-supplied strings (trigger char and names)
    - do not hook into the description text area - there is no code to send 
    emails out for mentions in there, only for journals

--- lib/redmine_mentions/journal_patch.rb
+++ lib/redmine_mentions/journal_patch.rb
@@ -3,13 +3,13 @@ module RedmineMentions
     def self.included(base)
       base.class_eval do
         after_create :send_mail
-        
+
         def send_mail
           if self.journalized.is_a?(Issue) && self.notes.present?
             issue = self.journalized
             project=self.journalized.project
             users=project.users.to_a.delete_if{|u| (u.type != 'User' || u.mail.empty?)}
-            users_regex=users.collect{|u| "\"#{Regexp.escape("#{u.firstname} #{u.lastname}")}\":#{Setting.protocol}://#{Setting.host_name}#{Rails.application.routes.url_helpers.user_path(u)}".gsub(/(["\/])/, '\\\\\1')}.join('|')
+            users_regex=users.collect{|u| "\"#{Regexp.escape("#{u.firstname} #{u.lastname}#{" - #{u.mail}" unless u.pref.hide_mail}")}\":#{Setting.protocol}://#{Setting.host_name}#{Rails.application.routes.url_helpers.user_path(u)}".gsub(/(["\/])/, '\\\\\1')}.join('|')
             regex_for_email = '\B('+users_regex+')\b'
             regex = Regexp.new(regex_for_email)
             mentioned_users = self.notes.scan(regex).uniq
